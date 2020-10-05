33c1326a25465f4ce4b7f70d2874252b5f75597a
jeetkumaryadav <jeetkishoreyadav@gmail.com>
2020-10-06 00:38:40 +0530

    [Jeet] JUnit Email Validation

--- src/main/java/com/capgemini/JUnit/Junit_UserValidation/App.java
+++ src/main/java/com/capgemini/JUnit/Junit_UserValidation/App.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class App {
+	private static Logger LOG = LogManager.getLogger(App.class);
 	static Scanner sc = new Scanner(System.in);
 
 	public boolean validateName(String name) {
@@ -14,16 +15,59 @@ public class App {
 		return validate;
 	}
 
-	private static Logger LOG = LogManager.getLogger(App.class);
-	
+	public boolean validateEmail(String email) {
+		boolean validate = Pattern
+				.matches("^[A-Za-z0-9]+(\\.[A-Za-z0-9-]+)?@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)?(\\.[A-Za-z]{2,})$", email);
+		return validate;
+	}
+
+	public boolean validateMobile(String mobile) {
+		boolean validate = Pattern.matches("[0-9]{2}[ ][0-9]{10}", mobile);
+		return validate;
+	}
+
+	public boolean validatePassword(String password) {
+		boolean validate = Pattern.matches("(?=^.{8,}$)(?=.[A-Z])(?=.[0-9])[a-zA-Z0-9]*([^(A-Za-z0-9 )]{1})[0-9a-zA-Z]*", password);
+		return validate;
+	}
+
 	public static void main(String[] args) {
 		App app = new App();
+
 		LOG.info("Enter the First Name:");
 		String firstName = sc.next();
-		while(!(app.validateName(firstName))){
+		while (!(app.validateName(firstName))) {
 			LOG.info("Invalid Entry. Enter the First Name:");
 			firstName = sc.next();
 		}
-	}
 
+		LOG.info("Enter the Last Name:");
+		String lastName = sc.next();
+		while (!(app.validateName(lastName))) {
+			LOG.info("Invalid Entry. Enter the Last Name:");
+			lastName = sc.next();
+		}
+
+		LOG.info("Enter the Email:");
+		String email = sc.next();
+		while (!(app.validateEmail(email))) {
+			LOG.info("Invalid Entry. Enter the Email:");
+			email = sc.next();
+		}
+        sc.nextLine();
+		LOG.info("Enter the Mobile Number:");
+		String mobile = sc.nextLine();
+		while (!(app.validateMobile(mobile))) {
+			LOG.info("Invalid Entry. Enter the Mobile Number:");
+			mobile = sc.next();
+		}
+
+		LOG.info("Enter the Password:");
+		String password = sc.next();
+		while (!(app.validateMobile(password))) {
+			LOG.info("Invalid Entry. Enter the Password:");
+			password = sc.next();
+		}
+
+}
 }
--- src/test/java/com/capgemini/JUnit/Junit_UserValidation/AppTest.java
+++ src/test/java/com/capgemini/JUnit/Junit_UserValidation/AppTest.java
@@ -1,38 +1,69 @@
 package com.capgemini.JUnit.Junit_UserValidation;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import java.util.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+@RunWith(Parameterized.class)
+public class AppTest {
+private boolean mailValid;
+private String Email;
+
+    static App app=null;
+    
+    public AppTest(String Email,boolean mailValid) {
+    	super();
+    	this.Email=Email;
+    	this.mailValid=mailValid;
+    	
+    }
+    
+    
+    @BeforeClass
+    public static void setUp(){
+    app=new App();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Parameterized.Parameters
+    public static Collection<Object[]> input() {
+    	return Arrays.asList(new Object[][] {{"abc@yahoo.com", true}, {"abc-100@yahoo.com",true},
+    			{"abc.100@yahoo.com",true},
+    			{"abc111@abc.com",true},
+    			{"ac.100@abc.com.au",true},
+    			{"abc@1.com",true},
+    			{"abc@gmail.com.com",true},
+    			{"abc+100@gmail.com",true}});
+    	
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    @Test
+    public void givenName_WhenProper_ShouldReturnTrue()
     {
-        assertTrue( true );
+        assertTrue("Invalid Name",app.validateName("Jeet"));
+        assertFalse("Valid Name",app.validateName("eet"));
+       
+    }
+    
+    @Test
+    public void givenEmail_WhenProper_ShouldReturnTrue()
+    {
+        assertTrue("Invalid Email",app.validateEmail(Email));
+        assertFalse("Valid Email",app.validateEmail("jeetkrkr@jee.qw12"));
+    }
+    
+    @Test
+    public void givenMobile_WhenProper_ShouldReturnTrue()
+    {
+        assertTrue("Invalid Mobile No",app.validateMobile("91 9471129295"));
+        assertFalse("Valid Mobile",app.validateMobile("7895622"));
+    }
+    
+    @Test
+    public void givenPassword_WhenProper_ShouldReturnTrue()
+    {
+        assertTrue("Invalid Password",app.validatePassword("mlnfhhhM112!"));
+        assertFalse("Valid Password",app.validatePassword("jeetkrkr@jee.qw12"));
     }
 }
